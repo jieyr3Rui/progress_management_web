8a302a6138e578fad1b4061eb34411c5b6aa45f5
jieyr3Rui <2930681195@qq.com>
2020-06-01 01:04:04 +0800

    fix bug 2

--- regist.php
+++ regist.php
@@ -2,14 +2,14 @@
     include "public.php";
     $name=$_REQUEST['uname'];
     $pass=$_REQUEST['pass'];
-    $sql="INSERT INTO 'user_table'('name', 'pass') VALUES ('$name', '$pass')";
+    $sql="INSERT INTO user_table(name, pass) VALUES ('$name', '$pass')";
     echo $sql;
-    $re=mysqli_query($conn, $sql);
+    $re=mysqli_query($conn, $sql); 
     if($re){
         echo "<script>alert('successful registion');location.href='login.html'</script>";    
     }
     else{
         echo "<script>alert('failure registion');location.href='regist.html'</script>";    
     }
-
+ 
 ?>
