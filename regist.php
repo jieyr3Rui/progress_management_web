8cf6845bba7d5c4294222b6ed8aa68f3cb22688c
jieyr3Rui <2930681195@qq.com>
2020-06-01 01:05:59 +0800

    run 1

--- regist.php
+++ regist.php
@@ -3,7 +3,6 @@
     $name=$_REQUEST['uname'];
     $pass=$_REQUEST['pass'];
     $sql="INSERT INTO user_table(name, pass) VALUES ('$name', '$pass')";
-    echo $sql;
     $re=mysqli_query($conn, $sql); 
     if($re){
         echo "<script>alert('successful registion');location.href='login.html'</script>";    
