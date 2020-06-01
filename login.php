f781138407291ceb0c5949fb135be2f247a67323
jieyr3Rui <2930681195@qq.com>
2020-06-01 10:24:49 +0800

    login

---
+++ login.php
@@ -0,0 +1,21 @@
+<?php
+    include "public.php";
+    $uname=$_REQUEST["uname"];
+    $pass=$_REQUEST["pass"];
+    $sql="SELECT name, pass FROM database_progress WHERE name='$uname'";
+    $re=mysqli_query($conn, $sql);
+    $n=mysqli_num_rows($re);
+    if(!$n){
+        echo "<script>alert('username does not exit');location.href='login.html'</script>";
+    }
+    else{
+        while($date=mysqli_fetch_assoc($re)){
+            if($date['name']==$uname&&date['pass']==$pass){
+                echo "<script>alert('seccessful login');location.href='main.html'</script>";
+            }
+            else{
+                echo "<script>alert('incorrect password');location.href='login.html'</script>";
+            }
+        }
+    }
+?>
