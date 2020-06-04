1a20fd0ff0747bbe6610ca7c935e887240ca0578
jieyr3Rui <2930681195@qq.com>
2020-06-04 19:32:13 +0800

    bug

--- test/draw.php
+++ test/draw.php
@@ -8,16 +8,16 @@ function op_width(){
     $dbname = "database_progress";
     $w = 10;
 
-    // 创建连接
-    $conn = new mysqli($servername, $username, $password, $dbname);
+    // // 创建连接
+    // $conn = new mysqli($servername, $username, $password, $dbname);
     
-    // Check connection
-    if ($conn->connect_error) {
-        die("连接失败: " . $conn->connect_error);
-    } 
-    else{
-        echo "successful";
-    }
+    // // Check connection
+    // if ($conn->connect_error) {
+    //     die("连接失败: " . $conn->connect_error);
+    // } 
+    // else{
+    //     echo "successful";
+    // }
     // $sql = "SELECT width FROM table_width WHERE pid='1234'";
     // $result = $conn->query($sql);
     
