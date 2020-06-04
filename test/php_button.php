dd7ccfd79d50cbeba91783716896954770238a44
jieyr3Rui <2930681195@qq.com>
2020-06-04 16:09:53 +0800

    php button

--- test/php_button.php
+++ test/php_button.php
@@ -3,11 +3,21 @@ function show(){
   echo "aaa";
 }
 ?>
- 
-<script language="JavaScript">
-function bt_click(){
-<?php show();?>
-}
-</script>
- 
-<input type="button" name="show" id="show" value="提交" onClick="bt_click();"/>
+
+<html>
+<head>
+<meta charset="utf-8">
+<title>main page</title>
+
+</head>
+
+<body>
+    <h1>我的第一个标题</h1>
+    <input type="button" name="show" id="show" value="提交" onClick="bt_click();"/>
+    <script>
+        function bt_click(){
+        <?php show();?>
+        }
+    </script>
+</body>
+</html>
