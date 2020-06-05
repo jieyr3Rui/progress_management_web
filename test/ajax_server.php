2f05fabbf640c5a3333af4077c11c8503c7a58d5
jieyr3Rui <2930681195@qq.com>
2020-06-05 12:26:09 +0800

    ajax test

--- test/ajax_server.php
+++ test/ajax_server.php
@@ -1,21 +1,112 @@
-<?php   
-    if (isset($_POST['action']))  
-    {  
-        switch($_POST['action'])  
-        {  
-            case "btn1":btn1();break;  
-            case "btn2":btn2();break;  
-            default:break;  
-        }  
-    }  
-      
-    function btn1()  
-    {  
-        echo "hello 按钮1";  
-    }  
-    function btn2()  
-    {  
-        echo "hello 按钮2";  
-    }  
-  
+<?php
+// 将姓名填充到数组中
+$a[]="Anna";
+$a[]="Brittany";
+$a[]="Cinderella";
+$a[]="Diana";
+$a[]="Eva";
+$a[]="Fiona";
+$a[]="Gunda";
+$a[]="Hege";
+$a[]="Inga";
+$a[]="Johanna";
+$a[]="Kitty";
+$a[]="Linda";
+$a[]="Nina";
+$a[]="Ophelia";
+$a[]="Petunia";
+$a[]="Amanda";
+$a[]="Raquel";
+$a[]="Cindy";
+$a[]="Doris";
+$a[]="Eve";
+$a[]="Evita";
+$a[]="Sunniva";
+$a[]="Tove";
+$a[]="Unni";
+$a[]="Violet";
+$a[]="Liza";
+$a[]="Elizabeth";
+$a[]="Ellen";
+$a[]="Wenche";
+$a[]="Vicky";
+
+//从请求URL地址中获取 q 参数
+$q=$_GET["q"];
+
+//查找是否由匹配值， 如果 q>0
+if (strlen($q) > 0)
+{
+    $hint="";
+    for($i=0; $i<count($a); $i++)
+    {
+        if (strtolower($q)==strtolower(substr($a[$i],0,strlen($q))))
+        {
+            if ($hint=="")
+            {
+                $hint=$a[$i];
+            }
+            else
+            {
+                $hint=$hint." , ".$a[$i];
+            }
+        }
+    }
+}
+
+// 如果没有匹配值设置输出为 "no suggestion" 
+if ($hint == "")
+{
+    $response="no suggestion";
+}
+else
+{
+    $response=$hint;
+}
+
+//输出返回值
+echo $response;
 ?>
+
+<html>
+<head>
+<script>
+function showHint(str)
+{
+    if (str.length==0)
+    { 
+        document.getElementById("txtHint").innerHTML="";
+        return;
+    }
+    if (window.XMLHttpRequest)
+    {
+        // IE7+, Firefox, Chrome, Opera, Safari 浏览器执行的代码
+        xmlhttp=new XMLHttpRequest();
+    }
+    else
+    {    
+        //IE6, IE5 浏览器执行的代码
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange=function()
+    {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200)
+        {
+            document.getElementById("txtHint").innerHTML=xmlhttp.responseText;
+        }
+    }
+    xmlhttp.open("GET","ajax_server.php?q="+str,true);
+    xmlhttp.send();
+}
+</script>
+</head>
+<body>
+
+<p><b>在输入框中输入一个姓名:</b></p>
+<form> 
+姓名: <input type="text" onkeyup="showHint(this.value)">
+</form>
+<p>返回值: <span id="txtHint"></span></p>
+
+</body>
+</html>
