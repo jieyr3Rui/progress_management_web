8c199374f407d898ce7ab36e8fa1f4445c946838
jieyr3Rui <2930681195@qq.com>
2020-06-05 15:03:58 +0800

    del pass

--- ajax_draw.php
+++ ajax_draw.php
@@ -1,10 +1,8 @@
 <?php
 
     header('Content-type:text/json;charset=utf-8');
-    $username=$_POST['username'];
-    $password=$_POST['password'];
     $width=$_POST['width'];
-    $data='{username:"' . $username . '",password:"' . $password .'",width:"'. $width. '"}';//组合成json格式数据
+    $data='{width:"'. $width. '"}';//组合成json格式数据
 
     echo json_encode($data);//输出json数据
 ?>
