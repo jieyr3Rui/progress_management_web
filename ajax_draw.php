2f2113f0633d5fcb5fc9af165dce464bfa98fa13
jieyr3Rui <2930681195@qq.com>
2020-06-05 14:39:39 +0800

    test

--- ajax_draw.php
+++ ajax_draw.php
@@ -1,11 +1,13 @@
 <?php
-    header('Content-type:text/json;charset=utf-8');
-    $width = intvar($_POST['width']);
-    $add = intvar($_POST['add']);
-    $total = $add + $width;
-    if($total >= 100){
-        $total = 100;
-    }
-    $data = '{new_width:"' . strval($total) . '"}'
-    echo json_encode($data);
+
+ header('Content-type:text/json;charset=utf-8');
+
+ $username=$_POST['username'];
+
+ $password=$_POST['password'];
+
+ $data='{username:"' . $username . '",password:"' . $password .'"}';//组合成json格式数据
+
+ echo json_encode($data);//输出json数据
+
 ?>
